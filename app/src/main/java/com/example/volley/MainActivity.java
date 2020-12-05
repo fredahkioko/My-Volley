69c0925867a86ad4bf1f492eee9e2e5431ef7372
Fredah <Fredah.kioko@strathmore.edu>
2020-12-05 16:16:55 +0300

    API call to git

---
+++ app/src/main/java/com/example/volley/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.volley;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //declare the recycler view
+    private RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Initialize the recycler view
+        recyclerView=findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
